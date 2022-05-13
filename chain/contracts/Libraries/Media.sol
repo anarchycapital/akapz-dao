731f70884547185ffc94569b84895b1ff0b0b80e
Quantos Blockchain & Network <virtusestaudax@protonmail.com>
2022-05-13 17:20:21 -0400

    updated media types

--- chain/contracts/Libraries/Media.sol
+++ chain/contracts/Libraries/Media.sol
@@ -31,6 +31,14 @@ library Media {
         uint _expiry;
     }
 
+    bytes32 public constant MEDIA_TYPE_IMAGE = keccak256(abi.encode("IMAGE"));
+    bytes32 public constant MEDIA_TYPE_VIDEO = keccak256(abi.encode("VIDEO"));
+    bytes32 public constant MEDIA_TYPE_PPMS = keccak256(abi.encode("PPMS"));
+    bytes32 public constant MEDIA_TYPE_PPVS = keccak256(abi.encode("PPMS"));
+    bytes32 public constant MEDIA_TYPE_FREEBIE_IMAGE = keccak256(abi.encode("FREEBIE_IMAGE"));
+    bytes32 public constant MEDIA_TYPE_FREEBIE_VIDEO = keccak256(abi.encode("FREEBIE_VIDEO"));
+    bytes32 public constant MEDIA_TYPE_FREEBIE_STREAM = keccak256(abi.encode("FREEBIE_STREAM"));
+
     function newImage(ImageDetails calldata _image) external  pure returns(ImageDetails memory) {
         return ImageDetails({
             mimeType: _image.mimeType,
--- chain/contracts/Nfts/draft-ERC721MetadataPayable.sol
+++ chain/contracts/Nfts/draft-ERC721MetadataPayable.sol
@@ -6,27 +6,52 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "../Libraries/Media.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+abstract contract ERC721MediaPayable is ERC721, ERC721URIStorage, Ownable {
 
-abstract contract ERC721MetadataPayable is Ownable {
+
+    address private _beneficiary;
+
+    event NFTBought(address indexed _nftToken, address indexed _from, address indexed _to, uint256 amount, address currency);
+    event NFTViewed(address indexed _nftToken, uint timestamp, bool _freeView);
+
+    uint private _views = 0;
+    uint private _freeViews = 0;
+    uint private _boughtNumTimes = 0;
+
 
     struct Metadata {
         address creator;
-        string mediaID;
         string ipfsBase;
         string previewImage;
         string previewVideo;
-        address itemAddress;
         bytes32 mediaType;
-        bool isBuyable;
-        bool oneTimePayment;
-        bool ppv;
-        bool liveStream;
-        bool unique;
-        uint qtyAvailable;
+    }
+
+    struct BoughtItem {
+        address _internalID;
+        string ipfsHash;
+        uint256 amount;
+        uint timestamp;
+        Metadata _itemMetas;
+    }
+
+    uint256 private price;
+    uint256 private priceType;
+
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _currentIds;
+
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://";
     }
 
     mapping (address => Metadata) private _metadataPayable;
-    mapping (address => bool) private _buyers;
+    mapping (address => bool) private _buyer;
+    mapping (address => ERC20) private _acceptedERC20Tokens;
+
 
     function Creator(address _nft) public view virtual returns (address) {
         return _getMetaDataPayable(_nft).creator;
@@ -38,6 +63,19 @@ abstract contract ERC721MetadataPayable is Ownable {
 
     function MediaType(address _nft) public view virtual returns (string) {
         return _getMetaDataPayable(_nft).mediaType;
+    }
+
+    function IsImageMediaType(address _nft) public view virtual returns(bool) {
+        return _getMetaDataPayable(_nft).mediaType == Media.MEDIA_TYPE_IMAGE || _getMetaDataPayable(_nft).mediaType == Media.MEDIA_TYPE_FREEBIE_IMAGE;
+    }
+
+    function IsVideoMediaType(address _nft) public view virtual returns(bool) {
+        return _getMetaDataPayable(_nft).mediaType == Media.MEDIA_TYPE_VIDEO|| _getMetaDataPayable(_nft).mediaType == Media.MEDIA_TYPE_FREEBIE_VIDEO;
+    }
+
+    function IsStreamMediaType(address _nft) public view virtual returns(bool) {
+        return _getMetaDataPayable(_nft).mediaType == Media.MEDIA_TYPE_FREEBIE_STREAM || _getMetaDataPayable(_nft).mediaType == Media.MEDIA_TYPE_PPMS ||
+        _getMetaDataPayable(_nft).mediaType == Media.MEDIA_TYPE_PPVS;
     }
 
 
@@ -60,9 +98,44 @@ abstract contract ERC721MetadataPayable is Ownable {
     function _afterGet() private {}
 
 
-    modifier onlyBuyer(address _sender) {
+    modifier onlyBuyer(uint _itemId, address _sender) {
         require(_buyers[_sender] == true, "please purchase this nft before doing this");
         _;
+    }
+
+
+    function safeMint(address to, string memory _uri) public onlyOwner {
+        uint256 tokenId = _tokenIds.current();
+        _tokenIds.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, _uri);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+    public
+    view
+    override(ERC721, ERC721URIStorage)
+    returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+
+    /*
+    @dev _buyAMint is when a token is buyable by customers to keep indefinitely for themselves for a ONE TIME FEE
+    */
+    function _buyAMint(address to, string memory metadataURI) public virtual payable returns (uint256) {
+        require(msg.value >= _price, "not enough value sent");
+        uint256 newItemId = _tokenIds.current();
+        _tokenIds.increment();
+        _existingURIs[metadataURI] = 1;
+        _mint(to, newItemId);
+        _setTokenURI(newItemId, metadataURI);
+        return newItemId;
     }
 
 }
